9f4be64091c071b5dc6739b7a26e8dd14b65db7e
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-06-09 01:52:43 +0900

    Add dump testcase

--- spec/ridgepole_spec.rb
+++
@@ -1,5 +0,0 @@
-describe Ridgepole::Client do
-  it do
-    expect(1).to eq(1)
-  end
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,26 @@
 require 'ridgepole'
 
+RSpec.configure do |config|
+  config.before(:each) do
+    restore_database
+  end
+end
+
 def restore_database
   sql_file = File.expand_path('../ridgepole_test.sql', __FILE__)
   system("mysql -uroot < #{sql_file}")
 end
 
-RSpec.configure do |config|
-  config.before(:each) do
-    restore_database
-  end
+def test_client(config = {}, options = {})
+  config = {
+    adapter: 'mysql2',
+    database: 'ridgepole_test',
+  }.merge(config)
+
+  options = {
+  }.merge(options)
+
+  client = Ridgepole::Client.new(config, options)
+
+  yield(client)
 end
