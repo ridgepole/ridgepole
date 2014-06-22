f5769587de05de609178f6525dc8e7bf48db86e1
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-06-22 18:44:52 +0900

    Fix spec

--- spec/diff/diff_empty_spec.rb
+++ spec/diff/diff_empty_spec.rb
@@ -1,9 +1,8 @@
 describe 'Ridgepole::Client#diff' do
   context 'when database is empty' do
-    subject { client }
-
-    it {
-      delta = subject.diff(<<-RUBY)
+    let(:actual_dsl) { '' }
+    let(:expected_dsl) {
+      <<-RUBY
         create_table "clubs", force: true do |t|
           t.string "name", default: "", null: false
         end
@@ -69,67 +68,16 @@ describe 'Ridgepole::Client#diff' do
 
         add_index "titles", ["emp_no"], name: "emp_no", using: :btree
       RUBY
+    }
 
+    subject { client }
+
+    it {
+      delta = subject.diff(expected_dsl)
       expect(delta.differ?).to be_true
-      expect(delta.script).to be_same_str_as(<<-RUBY)
-        create_table("clubs", {}) do |t|
-          t.string("name", {:default=>"", :null=>false})
-        end
-        add_index("clubs", ["name"], {:name=>"idx_name", :unique=>true, :using=>:btree})
-
-        create_table("departments", {:primary_key=>"dept_no"}) do |t|
-          t.string("dept_name", {:limit=>40, :null=>false})
-        end
-        add_index("departments", ["dept_name"], {:name=>"dept_name", :unique=>true, :using=>:btree})
-
-        create_table("dept_emp", {:id=>false}) do |t|
-          t.integer("emp_no", {:null=>false})
-          t.string("dept_no", {:limit=>4, :null=>false})
-          t.date("from_date", {:null=>false})
-          t.date("to_date", {:null=>false})
-        end
-        add_index("dept_emp", ["dept_no"], {:name=>"dept_no", :using=>:btree})
-        add_index("dept_emp", ["emp_no"], {:name=>"emp_no", :using=>:btree})
-
-        create_table("dept_manager", {:id=>false}) do |t|
-          t.string("dept_no", {:limit=>4, :null=>false})
-          t.integer("emp_no", {:null=>false})
-          t.date("from_date", {:null=>false})
-          t.date("to_date", {:null=>false})
-        end
-        add_index("dept_manager", ["dept_no"], {:name=>"dept_no", :using=>:btree})
-        add_index("dept_manager", ["emp_no"], {:name=>"emp_no", :using=>:btree})
-
-        create_table("employee_clubs", {}) do |t|
-          t.integer("emp_no", {:unsigned=>true, :null=>false})
-          t.integer("club_id", {:unsigned=>true, :null=>false})
-        end
-        add_index("employee_clubs", ["emp_no", "club_id"], {:name=>"idx_emp_no_club_id", :using=>:btree})
-
-        create_table("employees", {:primary_key=>"emp_no"}) do |t|
-          t.date("birth_date", {:null=>false})
-          t.string("first_name", {:limit=>14, :null=>false})
-          t.string("last_name", {:limit=>16, :null=>false})
-          t.string("gender", {:limit=>1, :null=>false})
-          t.date("hire_date", {:null=>false})
-        end
-
-        create_table("salaries", {:id=>false}) do |t|
-          t.integer("emp_no", {:null=>false})
-          t.integer("salary", {:null=>false})
-          t.date("from_date", {:null=>false})
-          t.date("to_date", {:null=>false})
-        end
-        add_index("salaries", ["emp_no"], {:name=>"emp_no", :using=>:btree})
-
-        create_table("titles", {:id=>false}) do |t|
-          t.integer("emp_no", {:null=>false})
-          t.string("title", {:limit=>50, :null=>false})
-          t.date("from_date", {:null=>false})
-          t.date("to_date", {})
-        end
-        add_index("titles", ["emp_no"], {:name=>"emp_no", :using=>:btree})
-      RUBY
+      expect(subject.dump).to be_same_str_as(actual_dsl)
+      delta.migrate
+      expect(subject.dump).to be_same_str_as(expected_dsl)
     }
   end
 end
