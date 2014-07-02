1300d41770d0c8287385fca6a8f65fa1fa3b352b
Genki Sugawara <sugawara@cookpad.com>
2014-07-02 16:54:51 +0900

    Add class methods to client

--- lib/ridgepole/client.rb
+++ lib/ridgepole/client.rb
@@ -28,4 +28,26 @@ class Ridgepole::Client
     logger.verbose_info('# Compare definitions')
     @diff.diff(current_definition, expected_definition)
   end
+
+  class << self
+    def diff(file_or_config1, file_or_config2, options = {})
+      definition1 = load_definition(file_or_config1)
+      definition2 = load_definition(file_or_config1)
+      diff = Ridgepole::Diff.new(options)
+      diff.diff(definition1, definition2)
+    end
+
+    def dump(conn_spec, options = {}, &block)
+      client = self.new(conn_spec, options)
+      client.dump(&block)
+    end
+
+    private
+
+    def load_definition(file_or_config, options = {})
+      definition = file_or_config.kind_of?(Hash) ? dump(file_or_config, options) : File.read(file)
+      parser = Ridgepole::DSLParser.new(options)
+      parser.parse(dsl)
+    end
+  end # of class methods
 end
