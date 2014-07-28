9839c35579a15dd8177b60bd1fa11458e1296282
Genki Sugawara <sugawara@cookpad.com>
2014-07-28 22:06:45 +0900

    Fix spec

--- spec/migrate/migrate_create_table_with_options_spec.rb
+++ spec/migrate/migrate_create_table_with_options_spec.rb
@@ -17,7 +17,13 @@ describe 'Ridgepole::Client#diff -> migrate' do
       delta = subject.diff(expected_dsl)
       expect(delta.differ?).to be_truthy
 
-      puts delta.script
+      expect(delta.script).to eq <<-RUBY.strip_heredoc.strip
+        create_table("employee_clubs", {:options=>"ENGINE=MyISAM CHARSET=utf8"}) do |t|
+          t.integer("emp_no", {:unsigned=>true, :null=>false})
+          t.integer("club_id", {:unsigned=>true, :null=>false})
+        end
+        add_index("employee_clubs", ["emp_no", "club_id"], {:name=>"idx_emp_no_club_id", :using=>:btree})
+      RUBY
     }
   end
 end
