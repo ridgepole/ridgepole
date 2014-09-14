98ee0a507ef53213a7edb04a664170d2a6849302
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-09-14 16:30:34 +0900

    Fix Ridgepole::Config

--- lib/ridgepole/config.rb
+++ lib/ridgepole/config.rb
@@ -1,16 +1,16 @@
-# -*- coding: utf-8 -*-
 require 'erb'
 require 'yaml'
 
-module Ridgepole::Config
-  def self.load(config, env='development')
+class Ridgepole::Config
+  def self.load(config, env = 'development')
     config = if File.exist?(config)
                yaml = ERB.new(File.read(config)).result
                YAML.load(yaml)
              else
                YAML.load(ERB.new(config).result)
              end
-    if config.has_key? env.to_s
+
+    if config.has_key?(env.to_s)
       config.fetch(env.to_s)
     else
       config
