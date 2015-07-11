4cd71013d5f6b97ecf0170c1574c696f548007e1
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2015-07-11 12:47:19 +0900

    Add spec

---
+++ spec/mysql/migrate/migrate_change_column_default_spec.rb
@@ -0,0 +1,151 @@
+unless postgresql?
+describe 'Ridgepole::Client#diff -> migrate' do
+  context 'when default:0 -> (emply)' do
+    let(:actual_dsl) {
+      <<-RUBY
+        create_table "salaries", id: false, force: :cascade do |t|
+          t.integer "emp_no",    limit: 4,  default: 0, null: false
+          t.float   "salary",    limit: 24,             null: false
+          t.date    "from_date",                        null: false
+          t.date    "to_date",                          null: false
+        end
+      RUBY
+    }
+
+    let(:expected_dsl) {
+      <<-RUBY
+        create_table "salaries", id: false, force: :cascade do |t|
+          t.integer "emp_no",    limit: 4,                null: true
+          t.float   "salary",    limit: 24,               null: false
+          t.date    "from_date",                          null: false
+          t.date    "to_date",                            null: false
+        end
+      RUBY
+    }
+
+    let(:result_dsl) {
+      <<-RUBY
+        create_table "salaries", id: false, force: :cascade do |t|
+          t.integer "emp_no",    limit: 4
+          t.float   "salary",    limit: 24, null: false
+          t.date    "from_date",            null: false
+          t.date    "to_date",              null: false
+        end
+      RUBY
+    }
+
+    before { subject.diff(actual_dsl).migrate }
+    subject { client }
+
+    it {
+      delta = subject.diff(expected_dsl)
+      expect(delta.differ?).to be_truthy
+      expect(subject.dump).to eq actual_dsl.strip_heredoc.strip
+      delta.migrate
+      expect(subject.dump).to eq result_dsl.strip_heredoc.strip
+    }
+  end
+
+  context 'when default:0 -> (emply with null:false)' do
+    let(:actual_dsl) {
+      <<-RUBY
+        create_table "salaries", id: false, force: :cascade do |t|
+          t.integer "emp_no",    limit: 4,  default: 0, null: false
+          t.float   "salary",    limit: 24,             null: false
+          t.date    "from_date",                        null: false
+          t.date    "to_date",                          null: false
+        end
+      RUBY
+    }
+
+    let(:expected_dsl) {
+      <<-RUBY
+        create_table "salaries", id: false, force: :cascade do |t|
+          t.integer "emp_no",    limit: 4,                null: false
+          t.float   "salary",    limit: 24,               null: false
+          t.date    "from_date",                          null: false
+          t.date    "to_date",                            null: false
+        end
+      RUBY
+    }
+
+    before { subject.diff(actual_dsl).migrate }
+    subject { client }
+
+    it {
+      delta = subject.diff(expected_dsl)
+      expect(delta.differ?).to be_truthy
+      expect(subject.dump).to eq actual_dsl.strip_heredoc.strip
+      delta.migrate
+      expect(subject.dump).to eq actual_dsl.strip_heredoc.strip
+    }
+  end
+
+  context 'when default:0 -> default:0' do
+    let(:actual_dsl) {
+      <<-RUBY
+        create_table "salaries", id: false, force: :cascade do |t|
+          t.integer "emp_no",    limit: 4,  default: 0, null: false
+          t.float   "salary",    limit: 24,             null: false
+          t.date    "from_date",                        null: false
+          t.date    "to_date",                          null: false
+        end
+      RUBY
+    }
+
+    before { subject.diff(actual_dsl).migrate }
+    subject { client }
+
+    it {
+      delta = subject.diff(actual_dsl)
+      expect(delta.differ?).to be_falsey
+    }
+  end
+
+  context 'when default:0 -> default:0/null:true' do
+    let(:actual_dsl) {
+      <<-RUBY
+        create_table "salaries", id: false, force: :cascade do |t|
+          t.integer "emp_no",    limit: 4,  default: 0, null: false
+          t.float   "salary",    limit: 24,             null: false
+          t.date    "from_date",                        null: false
+          t.date    "to_date",                          null: false
+        end
+      RUBY
+    }
+
+    let(:expected_dsl) {
+      <<-RUBY
+        create_table "salaries", id: false, force: :cascade do |t|
+          t.integer "emp_no",    limit: 4,  default: 0, null: true
+          t.float   "salary",    limit: 24,             null: false
+          t.date    "from_date",                        null: false
+          t.date    "to_date",                          null: false
+        end
+      RUBY
+    }
+
+    let(:result_dsl) {
+      <<-RUBY
+        create_table "salaries", id: false, force: :cascade do |t|
+          t.integer "emp_no",    limit: 4,  default: 0
+          t.float   "salary",    limit: 24,             null: false
+          t.date    "from_date",                        null: false
+          t.date    "to_date",                          null: false
+        end
+      RUBY
+    }
+
+    before { subject.diff(actual_dsl).migrate }
+    subject { client }
+
+    it {
+      delta = subject.diff(expected_dsl)
+      expect(delta.differ?).to be_truthy
+      expect(subject.dump).to eq actual_dsl.strip_heredoc.strip
+      delta.migrate
+      expect(subject.dump).to eq result_dsl.strip_heredoc.strip
+    }
+  end
+end
+end
