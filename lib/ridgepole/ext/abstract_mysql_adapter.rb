f22f166aa72762b11900aa80b04675c8efc708b8
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2015-11-21 01:52:04 +0900

    Add --mysql-use-alter spce

--- lib/ridgepole/ext/abstract_mysql_adapter.rb
+++ lib/ridgepole/ext/abstract_mysql_adapter.rb
@@ -5,7 +5,7 @@ class ActiveRecord::ConnectionAdapters::AbstractMysqlAdapter
     index_name, index_type, index_columns, index_options, index_algorithm, index_using = add_index_options(table_name, column_name, options)
 
     # cannot specify index_algorithm
-    execute "ALTER TABLE #{quote_table_name(table_name)} ADD INDEX #{quote_column_name(index_name)} #{index_type} #{index_using} (#{index_columns})#{index_options}"
+    execute "ALTER TABLE #{quote_table_name(table_name)} ADD #{index_type} INDEX #{quote_column_name(index_name)} #{index_using} (#{index_columns})#{index_options}"
   end
   alias_method_chain :add_index, :alter
 
