0b6ffc2d698cbad3df5605de0bfd49e07bb33179
winebarrel <sgwr_dts@yahoo.co.jp>
2017-07-03 22:49:25 +0900

    Add table options spec

--- spec/mysql/migrate/migrate_create_table_with_options_spec.rb
+++ spec/mysql/migrate/migrate_create_table_with_options_spec.rb
@@ -26,4 +26,32 @@ describe 'Ridgepole::Client#diff -> migrate' do
       EOS
     }
   end
+
+  context 'when create table (table definition options takes precedence)' do
+    let(:expected_dsl) {
+      <<-EOS
+        create_table "employee_clubs", force: :cascade, options: "ENGINE=InnoDB CHARSET=utf8mb4" do |t|
+          t.integer "emp_no", null: false, unsigned: true
+          t.integer "club_id", null: false, unsigned: true
+        end
+
+        add_index "employee_clubs", ["emp_no", "club_id"], name: "idx_emp_no_club_id", using: :btree
+      EOS
+    }
+
+    subject { client(table_options: 'ENGINE=MyISAM CHARSET=utf8') }
+
+    it {
+      delta = subject.diff(expected_dsl)
+      expect(delta.differ?).to be_truthy
+
+      expect(delta.script).to match_fuzzy <<-EOS
+        create_table("employee_clubs", {:options=>"ENGINE=InnoDB CHARSET=utf8mb4"}) do |t|
+          t.column("emp_no", :"integer", {:null=>false, :unsigned=>true, :limit=>4})
+          t.column("club_id", :"integer", {:null=>false, :unsigned=>true, :limit=>4})
+        end
+        add_index("employee_clubs", ["emp_no", "club_id"], {:name=>"idx_emp_no_club_id", :using=>:btree})
+      EOS
+    }
+  end
 end
