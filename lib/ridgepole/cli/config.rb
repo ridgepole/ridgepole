152332076688fc9fc8dff0284655972641d08f38
Genki Sugawara <sugawara@cookpad.com>
2017-07-21 16:09:47 +0900

    Fix db url check

--- lib/ridgepole/cli/config.rb
+++ lib/ridgepole/cli/config.rb
@@ -37,7 +37,7 @@ class Ridgepole::Config
     def parse_database_url(config)
       uri = URI.parse(config)
 
-      if [uri.scheme, uri.user, uri.host, uri.path].any? {|i| i.nil? }
+      if [uri.scheme, uri.user, uri.host, uri.path].any? {|i| i.nil? or i.empty? }
         raise "Invalid config: #{config.inspect}"
       end
 
