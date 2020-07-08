299bfcc5a41fd75be46208dc09fd2e077d9521a9
Kohei Suzuki <eagletmt@gmail.com>
2020-07-08 21:53:11 +0900

    Supress kwargs warnings from old Rails

--- spec/mysql/collation/collation_spec.rb
+++ spec/mysql/collation/collation_spec.rb
@@ -138,6 +138,12 @@ describe 'Ridgepole::Client#diff -> migrate' do
           opts = ['--dump-without-table-options']
           out, status = run_ridgepole('--diff', "'#{JSON.dump(conn_spec)}'", f.path, *opts)
 
+          # v6.0.3 is the oldest version that doesn't produce any kwargs warnings with Ruby 2.7
+          if condition('< 6.0.3')
+            # https://github.com/jeremyevans/ruby-warning/blob/1.1.0/lib/warning.rb#L18
+            out = out.lines.grep_v(/: warning: (?:Using the last argument (?:for `.+' )?as keyword parameters is deprecated; maybe \*\* should be added to the call|Passing the keyword argument (?:for `.+' )?as the last hash parameter is deprecated|Splitting the last argument (?:for `.+' )?into positional and keyword parameters is deprecated|The called method (?:`.+' )?is defined here)\n\z/).join('')
+          end
+
           expect(out).to be_empty
           expect(status.success?).to be_truthy
         end
