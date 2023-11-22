fb3786693e8ae306123f44ad02d3d8f848795348
alpaca-tc <alpaca-tc@alpaca.tc>
2023-11-22 13:43:30 +0900

    Fixed a bug that caused renamed_from to error as an invalid option.
    Fixed: #452
    
    Co-authored-by: Susumu UEKIHARA <uekkee@gmail.com>

--- spec/mysql/migrate/migrate_rename_column_spec.rb
+++ spec/mysql/migrate/migrate_rename_column_spec.rb
@@ -181,4 +181,30 @@ describe 'Ridgepole::Client#diff -> migrate' do
       end.to raise_error('Column `age` not found')
     }
   end
+
+  context 'when renamed_from column option is set for new table' do
+    let(:actual_dsl) do
+      erbh(<<-ERB)
+        create_table "clubs", force: :cascade do |t|
+          t.string "name", renamed_from: "name_old"
+        end
+      ERB
+    end
+
+    let(:expected_dsl) do
+      erbh(<<-ERB)
+        create_table "clubs", force: :cascade do |t|
+          t.string "name"
+        end
+      ERB
+    end
+
+    subject { client }
+
+    before { subject.diff(actual_dsl).migrate }
+
+    it 'ignores renamed_form column option' do
+      expect(subject.dump).to match_ruby expected_dsl
+    end
+  end
 end
