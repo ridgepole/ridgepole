e0b1a90c8c81238c0d44b6875b9e6e71bcd409ed
Kensaku Araga <k_araga@ivry.jp>
2025-07-22 11:35:07 +0900

    fix: support ruby3.4

--- spec/postgresql/diff/diff_spec.rb
+++ spec/postgresql/diff/diff_spec.rb
@@ -241,7 +241,7 @@ describe 'Ridgepole::Client.diff' do
       it 'creates index with algorithm: :concurrently' do
         delta = subject.diff(actual_dsl, expected_dsl)
         expect(delta).to be_differ
-        expect(delta.script).to include('add_index("users", ["email"], **{:name=>"idx_users_email", :algorithm=>:concurrently})')
+        expect(delta.script).to match(/add_index\("users", \["email"\], .*algorithm.*concurrently.*\)/)
       end
     end
     context 'when index exists without algorithm' do
