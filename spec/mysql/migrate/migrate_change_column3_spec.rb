2e030304bca6d996c864ec15095e1cdd0d6279db
winebarrel <sugawara@winebarrel.jp>
2025-10-29 17:42:15 +0900

    WIP

--- spec/mysql/migrate/migrate_change_column3_spec.rb
+++ spec/mysql/migrate/migrate_change_column3_spec.rb
@@ -55,9 +55,9 @@ describe 'Ridgepole::Client#diff -> migrate' do
       <<-RUBY
         create_table "employees", primary_key: "emp_no", force: :cascade do |t|
           t.date   "birth_date", null: false
-          t.date   "hire_date", null: false
           t.string "first_name", limit: 14, null: false
           t.string "gender", limit: 1, null: false
+          t.date   "hire_date", null: false
           t.string "last_name", limit: 16, null: false
           t.timestamps
         end
@@ -96,9 +96,9 @@ describe 'Ridgepole::Client#diff -> migrate' do
         create_table "employees", primary_key: "emp_no", force: :cascade do |t|
           t.date   "birth_date", null: false
           t.string "first_name", limit: 14, null: false
-          t.string "last_name", limit: 16, null: false
           t.string "gender", limit: 1, null: false
           t.date   "hire_date", null: false
+          t.string "last_name", limit: 16, null: false
           t.bigint "user_id"
           t.index "user_id"
         end
@@ -110,9 +110,9 @@ describe 'Ridgepole::Client#diff -> migrate' do
         create_table "employees", primary_key: "emp_no", force: :cascade do |t|
           t.date   "birth_date", null: false
           t.string "first_name", limit: 14, null: false
-          t.string "last_name", limit: 16, null: false
           t.string "gender", limit: 1, null: false
           t.date   "hire_date", null: false
+          t.string "last_name", limit: 16, null: false
           t.references :user
         end
       RUBY
@@ -133,9 +133,9 @@ describe 'Ridgepole::Client#diff -> migrate' do
         create_table "employees", primary_key: "emp_no", force: :cascade do |t|
           t.date   "birth_date", null: false
           t.string "first_name", limit: 14, null: false
-          t.string "last_name", limit: 16, null: false
           t.string "gender", limit: 1, null: false
           t.date   "hire_date", null: false
+          t.string "last_name", limit: 16, null: false
           t.bigint "user_id"
           t.string "user_type"
           t.index ["user_type", "user_id"]
@@ -148,9 +148,9 @@ describe 'Ridgepole::Client#diff -> migrate' do
         create_table "employees", primary_key: "emp_no", force: :cascade do |t|
           t.date   "birth_date", null: false
           t.string "first_name", limit: 14, null: false
-          t.string "last_name", limit: 16, null: false
           t.string "gender", limit: 1, null: false
           t.date   "hire_date", null: false
+          t.string "last_name", limit: 16, null: false
           t.references :user, polymorphic: true
         end
       RUBY
