31ae13d725d6764c0d75311a47ecd943b4bc36d2
Kenta Ishizaki <kentaishizaki@55728.jp>
2026-03-20 23:25:36 +0900

    Fix auto_increment column creation when index is added separately
    When a non-primary-key column has auto_increment and an index is defined on it, MySQL requires the index to exist at CREATE TABLE time. Ridgepole's default behavior separates CREATE TABLE and ADD INDEX into separate queries, causing:
    
      Mysql2::Error: Incorrect table definition; there can be only one auto column and it must be defined as a key
    
    Automatically include indices referencing auto_increment columns inside the CREATE TABLE block on MySQL, without requiring the --create-table-with-index option.
    
    Closes #494

---
+++ spec/mysql/~dump_auto_increment/migrate_create_auto_increment_with_index_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+describe 'Ridgepole::Client#diff -> migrate' do
+  context 'when create table with non-PK auto_increment column and index (without --create-table-with-index)' do
+    let(:actual_dsl) { '' }
+
+    let(:expected_dsl) do
+      erbh(<<-ERB)
+        create_table "sample_table", id: { type: :string, limit: 26 }, force: :cascade do |t|
+          t.datetime "created_at", null: false
+          t.bigint "partition_key", null: false, unsigned: true, auto_increment: true
+          t.datetime "updated_at", null: false
+          t.index ["partition_key", "id"], name: "idx_partition_key_and_id", unique: true
+        end
+      ERB
+    end
+
+    before { subject.diff(actual_dsl).migrate }
+    subject { client(mysql_dump_auto_increment: true) }
+
+    it {
+      delta = subject.diff(expected_dsl)
+      expect(delta.differ?).to be_truthy
+
+      # The auto_increment column's index should be inside CREATE TABLE
+      expect(delta.script).to match_fuzzy erbh(<<-ERB)
+        create_table("sample_table", **#{
+          { id: { type: :string, limit: 26 } }
+        }) do |t|
+          t.column("created_at", :"datetime", **#{{ null: false }})
+          t.column("partition_key", :"bigint", **#{
+            { null: false, unsigned: true, auto_increment: true, limit: 8 }
+          })
+          t.column("updated_at", :"datetime", **#{{ null: false }})
+          t.index(["partition_key", "id"], **#{{ name: 'idx_partition_key_and_id', unique: true }})
+        end
+      ERB
+
+      expect(subject.dump).to match_ruby actual_dsl
+      delta.migrate
+      expect(subject.dump).to match_ruby expected_dsl
+    }
+  end
+end
